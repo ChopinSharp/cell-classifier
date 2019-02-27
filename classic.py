24458a5332d63241ba074b092145d4604d011c1b
Meng5th <chopinsharp@163.com>
2019-02-27 18:20:28 +0800

    first commit

---
+++ classic.py
@@ -0,0 +1,121 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import svm
+import os
+import pandas as pd
+from tqdm import tqdm
+import time
+
+
+# Wrapper of the feature extraction function to keep the flag consistent for train, val, and test dataset.
+def feature_extractor(max_num_kp=100, enable_enhance=True, enable_warning=True, show_features=False):
+    def _extract_features(data_dir):
+        file_names = os.listdir(data_dir)
+        num_samples = len(file_names)
+        sift = cv2.xfeatures2d.SIFT_create(nfeatures=max_num_kp)
+        features = np.zeros((num_samples, max_num_kp, 128), dtype=np.float32)
+        labels = np.zeros(num_samples, dtype=np.int8)
+
+        warning_log = []
+
+        time.sleep(0.5)  # pause to show progress bar
+        for idx, name in enumerate(tqdm(file_names, desc='extracting features from ' + data_dir)):
+            labels[idx] = int(name[0])
+            img = cv2.imread(os.path.join(data_dir, name), cv2.IMREAD_GRAYSCALE)
+            kp, feature = sift.detectAndCompute(img, None)
+            # Enhance image if not enough key points found
+            if enable_enhance and len(kp) < max_num_kp:
+                img_enhanced = cv2.equalizeHist(img)
+                kp, feature = sift.detectAndCompute(img_enhanced, None)
+            num_kp = len(kp)
+            if num_kp == 0:
+                warning_log.append(' * Unable to extract features for %s' % name)
+                labels[idx] = -1  # mark as UNRECOGNIZABLE
+                continue
+            elif num_kp < max_num_kp:
+                warning_log.append(' + Not enough key points found in %s : %d/%d!' % (name, len(kp), max_num_kp))
+            num_kp = min(num_kp, max_num_kp)
+            features[idx][:num_kp] = feature[:num_kp]
+            if show_features:
+                img_with_kp = cv2.drawKeypoints(img, kp, np.array([]), flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+                plt.imshow(img_with_kp)
+                plt.show()
+        time.sleep(0.5)  # pause to show progress bar
+
+        if enable_warning:
+            for line in warning_log:
+                print(line)
+
+        return features.reshape(num_samples, -1), labels
+    return _extract_features
+
+
+def compute_accuracy(y_gt, y_p):
+    return np.sum(y_gt == y_p) / y_gt.shape[0]
+
+
+def validate_models(C_candidates, gamma_candidates, kernel='linear'):
+    best_acc = -1
+    best_models = None
+    best_params = None
+    train_acc_tbl = []
+    val_acc_tbl = []
+
+    time.sleep(0.5)  # pause to show progress bar
+    with tqdm(total=len(C_candidates) * len(gamma_candidates), desc='tuning hyper-parameters') as pbar:
+        for C in C_candidates:
+            train_acc_row = []
+            val_acc_row = []
+            for gamma in gamma_candidates:
+                classifier = svm.SVC(C=C, kernel=kernel, gamma=gamma)
+                classifier.fit(train_fts, train_labels)
+                preds = classifier.predict(train_fts)
+                train_acc = compute_accuracy(train_labels, preds)
+                preds = classifier.predict(val_fts)
+                val_acc = compute_accuracy(val_labels, preds)
+                # print('C = %.3e, gamma = %.3e, train_acc: %.3f%%, val_acc: %.3f%%' %
+                #     (C, gamma, 100 * train_acc, 100 * val_acc))
+                train_acc_row.append(100 * train_acc)
+                val_acc_row.append(100 * val_acc)
+                if val_acc > best_acc:
+                    best_acc = val_acc
+                    best_models = [classifier]
+                    best_params = [(C, gamma)]
+                elif val_acc == best_acc:
+                    best_models.append(classifier)
+                    best_params.append((C, gamma))
+                # Update progress bar
+                pbar.update()
+            train_acc_tbl.append(train_acc_row)
+            val_acc_tbl.append(val_acc_row)
+    time.sleep(0.5)  # pause to show progress bar
+
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.max_rows', None)
+    print('\nTrain Acc:')
+    print(pd.DataFrame(data=train_acc_tbl, index=gamma_candidates, columns=C_candidates))
+    print('Val Acc:')
+    print(pd.DataFrame(data=val_acc_tbl, index=gamma_candidates, columns=C_candidates))
+    return best_acc, best_params, best_models
+
+
+base_dir = 'data0229'
+
+# Extract features
+print('Extracting features ...')
+extract_features = feature_extractor(enable_enhance=False)
+train_fts, train_labels = extract_features(os.path.join(base_dir, 'train'))
+val_fts, val_labels = extract_features(os.path.join(base_dir, 'val'))
+test_fts, test_labels = extract_features(os.path.join(base_dir, 'test'))
+
+# Validate & Test models
+print('\nValidating model ...')
+val_acc, params, models = validate_models(np.logspace(-10, 10, 7), 1 / np.logspace(-10, 10, 7), kernel='linear')
+print()
+for best_param, best_model in zip(params, models):
+    preds = best_model.predict(test_fts)
+    test_acc = compute_accuracy(test_labels, preds)
+    stats = (*best_param, 100 * val_acc, 100 * test_acc)
+    print('Found best model with C = %.3e, gamma = %.3e, val_acc %.3f%%, Test accuracy: %.3f%%' % stats)
+
---
+++ transfer_learning.py
@@ -0,0 +1,341 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time, os, copy
+import PIL
+
+
+# Top level data directory.
+data_dir = 'data0229'
+
+# Models to choose from [resnet, alexnet, vgg, squeezenet, densenet]
+model_names = ['resnet', 'alexnet', 'vgg', 'squeezenet', 'densenet', 'inception']
+
+# Number of classes in the dataset
+num_classes = 3
+
+# Batch size for training (change depending on how much memory you have)
+batch_size = 4
+
+# Number of epochs to train for
+num_epochs = 20
+
+# Flag for feature extracting.
+feature_extract = True
+
+# Device to use in training
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+def set_parameter_requires_grad(model, feature_extracting):
+    if feature_extracting:
+        for param in model.parameters():
+            param.requires_grad = False
+
+
+def initialize_model(model_name, num_classes, feature_extract, use_pretrained=True, show_model=False):
+    # Initialize these variables which will be set in this if statement. Each of these variables is model specific.
+    model_ft = None
+    input_size = 0
+
+    if model_name == "resnet":
+        """ Resnet18
+        """
+        model_ft = models.resnet18(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.fc.in_features
+        model_ft.fc = nn.Linear(num_ftrs, num_classes)
+        input_size = 224
+
+    elif model_name == "alexnet":
+        """ Alexnet
+        """
+        model_ft = models.alexnet(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.classifier[6].in_features
+        model_ft.classifier[6] = nn.Linear(num_ftrs,num_classes)
+        input_size = 224
+
+    elif model_name == "vgg":
+        """ VGG11_bn
+        """
+        model_ft = models.vgg11_bn(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.classifier[6].in_features
+        model_ft.classifier[6] = nn.Linear(num_ftrs,num_classes)
+        input_size = 224
+
+    elif model_name == "squeezenet":
+        """ Squeezenet
+        """
+        model_ft = models.squeezenet1_0(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        model_ft.classifier[1] = nn.Conv2d(512, num_classes, kernel_size=(1,1), stride=(1,1))
+        model_ft.num_classes = num_classes
+        input_size = 224
+
+    elif model_name == "densenet":
+        """ Densenet
+        """
+        model_ft = models.densenet121(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.classifier.in_features
+        model_ft.classifier = nn.Linear(num_ftrs, num_classes)
+        input_size = 224
+
+    elif model_name == "inception":
+        """ Inception v3
+        Be careful, expects (299,299) sized images and has auxiliary output
+        """
+        model_ft = models.inception_v3(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        # Handle the auxilary net
+        num_ftrs = model_ft.AuxLogits.fc.in_features
+        model_ft.AuxLogits.fc = nn.Linear(num_ftrs, num_classes)
+        # Handle the primary net
+        num_ftrs = model_ft.fc.in_features
+        model_ft.fc = nn.Linear(num_ftrs, num_classes)
+        input_size = 299
+
+    else:
+        print("Invalid model name, exiting...")
+        exit()
+
+    if show_model:
+        print(model_ft)
+
+    return model_ft, input_size
+
+
+def create_dataloaders(data_dir, input_size):
+    # Calculate dataset mean and std
+    # dataset_mean = [0.485, 0.456, 0.406]
+    # dataset_std = [0.229, 0.224, 0.225]
+    print('Calculating dataset mean and standard deviation ...')
+    dataset = torch.utils.data.ConcatDataset([
+        datasets.ImageFolder(
+            os.path.join(data_dir, x),
+            transforms.Compose([
+                transforms.Resize((input_size, input_size)),
+                transforms.ToTensor()
+            ])
+        )
+        for x in ['train', 'val', 'test']
+    ])
+    loader = torch.utils.data.DataLoader(dataset, batch_size=10, num_workers=5, drop_last=True)
+    num_batches = len(dataset) // 10
+    dataset_mean = torch.zeros(3)
+    dataset_std = torch.zeros(3)
+    for inputs, _ in iter(loader):
+        dataset_mean += inputs.mean(dim=(0, 2, 3))
+    dataset_mean /= num_batches
+    for inputs, _ in iter(loader):
+        dataset_std += torch.mean((inputs - dataset_mean.reshape((1, 3, 1, 1))) ** 2, dim=(0, 2, 3))
+    dataset_std = torch.sqrt(dataset_std / num_batches)
+    print('Mean:', dataset_mean)
+    print('Deviation', dataset_std)
+
+    # Data augmentation and normalization for training
+    # Just normalization for validation
+    data_transforms = {
+        'train': transforms.Compose([
+            transforms.RandomRotation(45, resample=PIL.Image.BILINEAR),
+            transforms.RandomResizedCrop(input_size),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomVerticalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize(dataset_mean, dataset_std)
+        ]),
+        'val': transforms.Compose([
+            # transforms.Resize(256),
+            transforms.CenterCrop(input_size),
+            transforms.ToTensor(),
+            transforms.Normalize(dataset_mean, dataset_std)
+        ]),
+        'test': transforms.Compose([
+            # transforms.Resize(256),
+            transforms.CenterCrop(input_size),
+            transforms.ToTensor(),
+            transforms.Normalize(dataset_mean, dataset_std)
+        ])
+    }
+
+    # Create training and validation datasets
+    image_datasets = {
+        x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x])
+        for x in ['train', 'val', 'test']
+    }
+
+    # Create training and validation dataloaders
+    dataloaders = {
+        x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4, shuffle=True, num_workers=1)
+        for x in ['train', 'val', 'test']
+    }
+
+    # class_names = image_datasets['train'].classes
+    dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val', 'test']}
+    print('%(train)d samples for training, %(val)d for validation, %(test)d for test' % dataset_sizes)
+
+    return dataloaders
+
+
+def train_model(model, dataloaders, criterion, optimizer, num_epochs=25, is_inception=False):
+    since = time.time()
+    val_acc_history = []
+    loss_history = []
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print('Epoch %d/%d' % (epoch + 1, num_epochs))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward, track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    if is_inception and phase == 'train':
+                        # From https://discuss.pytorch.org/t/how-to-optimize-inception-model-with-auxiliary-classifiers/7958
+                        outputs, aux_outputs = model(inputs)
+                        loss1 = criterion(outputs, labels)
+                        loss2 = criterion(aux_outputs, labels)
+                        loss = loss1 + 0.4*loss2
+
+
+                    else:
+                        outputs = model(inputs)
+                        loss = criterion(outputs, labels)
+
+                    _, preds = torch.max(outputs, 1)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss / len(dataloaders[phase].dataset)
+            epoch_acc = running_corrects.double() / len(dataloaders[phase].dataset)
+
+            print('%s Loss: %.4f Acc: %.4f' % (phase, epoch_loss, epoch_acc))
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+            if phase == 'val':
+                val_acc_history.append(epoch_acc)
+                loss_history.append(epoch_loss)
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in %.0fm %.0fs' % (time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: %4f' % best_acc)
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model, val_acc_history, loss_history
+
+
+def test_model(model, dataloaders, is_inception=False):
+    model.eval()
+    running_corrects = 0
+
+    # Iterate over data.
+    for inputs, labels in dataloaders['test']:
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+
+
+        # forward, track history if only in train
+        with torch.no_grad():
+            if is_inception:
+                outputs, _ = model(inputs)
+            else:
+                outputs = model(inputs)
+            _, preds = torch.max(outputs, 1)
+
+        # statistics
+        running_corrects += torch.sum(preds == labels.data)
+
+    test_acc = running_corrects.double() / len(dataloaders['test'].dataset)
+    return test_acc
+
+
+for model_name in model_names:
+    print('Using', model_name)
+
+    # Initialize the model for this run
+    model_ft, input_size = initialize_model(model_name, num_classes, feature_extract, use_pretrained=True)
+
+    # Send the model to GPU
+    model_ft = model_ft.to(device)
+
+    # Create dataloaders
+    dataloaders = create_dataloaders(data_dir, input_size)
+
+    # Gather the parameters to be optimized/updated in this run. If we are
+    #  finetuning we will be updating all parameters. However, if we are
+    #  doing feature extract method, we will only update the parameters
+    #  that we have just initialized, i.e. the parameters with requires_grad
+    #  is True.
+    print("Params to learn:")
+    if feature_extract:
+        params_to_update = []
+        for name, param in model_ft.named_parameters():
+            if param.requires_grad:
+                params_to_update.append(param)
+                print("\t",name)
+    else:
+        params_to_update = model_ft.parameters()
+        for name,param in model_ft.named_parameters():
+            if param.requires_grad:
+                print("\t",name)
+
+    # Observe that all parameters are being optimized
+    # optimizer_ft = optim.SGD(params_to_update, lr=0.001, momentum=0.9)
+    optimizer_ft = optim.Adam(params_to_update, lr=0.001, betas=(0.9, 0.999), eps=1e-08, weight_decay=0, amsgrad=False)
+
+    # Setup the loss fxn
+    criterion = nn.CrossEntropyLoss()
+
+    # Train and evaluate
+    model_ft, val_acc_history, loss_history = train_model(model_ft, dataloaders, criterion, optimizer_ft,
+                                                    num_epochs=num_epochs, is_inception=(model_name == "inception"))
+    plt.subplot(211)
+    plt.xlabel('epoch')
+    plt.ylabel('val_acc')
+    plt.plot(val_acc_history)
+    plt.subplot(212)
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
+    plt.plot(loss_history)
+    plt.show()
+
+    # Test model
+    test_acc = test_model(model_ft, dataloaders)
+    print('Test Acc: %.4f' % test_acc)
+
+
