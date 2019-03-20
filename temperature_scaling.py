Bug fix: temperature scaling with new data loading pipeline Fix temperature scaling fail to optimize temperature, add sanity check to fall back.

temperature_scaling.py
@@ -4,7 +4,7 @@ from torch.nn import functional as F
 
 
 def compute_temperature(model, val_loader=None, verbose=False):
-    temperature = torch.tensor(1.5, requires_grad=True)
+    temperature = torch.tensor(1.0, requires_grad=True)
 
     # Declare criterions
     nll_criterion = nn.CrossEntropyLoss()
@@ -27,23 +27,32 @@ def compute_temperature(model, val_loader=None, verbose=False):
         loss.backward()
         return loss
 
-    optimizer = optim.LBFGS([temperature], lr=0.01, max_iter=50)
+    optimizer = optim.LBFGS([temperature], lr=0.001, max_iter=70)
     optimizer.step(__update)
 
     # Stop tracking gradient for temperature parameter
     temperature.requires_grad_(False)
 
+    nll_before_scaling = nll_criterion(logits_val, labels_val).item()
+    ece_before_scaling = ece_criterion(logits_val, labels_val).item()
+    nll_after_scaling = nll_criterion(logits_val.div(temperature), labels_val).item()
+    ece_after_scaling = ece_criterion(logits_val.div(temperature), labels_val).item()
+    opt_temperature = temperature.item()
+
+    # Fallback
+    if nll_after_scaling > nll_before_scaling or ece_after_scaling > ece_before_scaling \
+            or opt_temperature < 1e-2 or opt_temperature > 100:
+        if verbose:
+            print('+ Fail to optimize temperature, falling back ...')
+        return 1.0
+
     # Print out stats if verbose flag is set
     if verbose:
-        nll_before_scaling = nll_criterion(logits_val, labels_val).item()
-        ece_before_scaling = ece_criterion(logits_val, labels_val).item()
-        nll_after_scaling = nll_criterion(logits_val.div(temperature), labels_val).item()
-        ece_after_scaling = ece_criterion(logits_val.div(temperature), labels_val).item()
-        print('+ Optimal temperature: %.3f' % temperature.item())
+        print('+ Optimal temperature: %.3f' % opt_temperature)
         print('+ Before temperature scaling - NLL: %.3f, ECE: %.3f' % (nll_before_scaling, ece_before_scaling))
         print('+ After temperature scaling - NLL: %.3f, ECE: %.3f' % (nll_after_scaling, ece_after_scaling))
 
-    return temperature.item()
+    return opt_temperature
 
 
 class _ECELoss(nn.Module):

transfer_learning.py
@@ -496,14 +496,14 @@ def train(data_dir, model_name='squeezenet', model_dir=None, plot_dir=None, num_
 # def test_temperature_scaling():
 #     from temperature_scaling_ref import ModelWithTemperature
 #     # Load saved model
-#     model_dir = 'models'
+#     model_dir = 'modules'
 #     model_file_name = os.listdir(model_dir)[0]  # load first model file by default
-#     model_name = model_file_name.split('%')[0]
+#     model_name = 'squeezenet'
 #     original_model, _ = initialize_model(model_name, num_classes=3, feature_extract=True)
 #     original_model.load_state_dict(torch.load(os.path.join(model_dir, model_file_name)))
 #     original_model.eval()
 #
-#     data_dir = 'data0229_dp'
+#     data_dir = 'data0229'
 #     batch_size = 4
 #     dataloaders, dataset_mean, dataset_std = \
 #         create_dataloaders(data_dir, available_models_input_size[model_name], batch_size)
@@ -554,7 +554,7 @@ def convert_to_onnx_model():
 
 
 if __name__ == '__main__':
-    train('./data0318', model_name='squeezenet', num_epochs=30, model_dir='models')
+    # train('data0229', model_name='squeezenet', num_epochs=30, model_dir='models')
     # visualize_model()
     # test_temperature_scaling()
     # convert_to_torch_script()